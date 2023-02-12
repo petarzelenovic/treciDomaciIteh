064f4d6e1baa504e67a7d117bf5d4cc42ca0b8ec
Petar Zelenovic <petarzelenovic261@gmail.com>
2023-02-12 21:17:46 +0100

    izmene

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import Logo from '../assets/images/Logo.png';
 const Footer = () => {
   return (
     <Box mt="80px" bgcolor="#FFF3F4">
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+      <img src={Logo} alt="logo" style={{ width: '120px', height: '100px' }} />
     </Stack>
-    <Typography  sx={{ fontSize: { lg: '15px', xs: '8px' } }} mt="41px" textAlign="center" pb="40px">Everything about fitness in one place</Typography>
+    <Typography  sx={{ fontSize: { lg: '15px', xs: '8px' } }} mt="10px" textAlign="center" pb="40px">Everything about fitness in one place</Typography>
   </Box>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ const Navbar = () => {
   return (
     <Stack direction="row"  sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px"> 
       <Link to="/">
-      <img src={Logo} alt="Logo" style={{width:'48px', height:'48px', margin:'0 20px'}}/>
+      <img src={Logo} alt="Logo" style={{width:'125px', height:'100px', margin:'0 20px'}}/>
       </Link>
       
         <Stack  direction="row"
         gap="40px"
         fontFamily="Alegreya"
         fontSize="24px"
-        alignItems="flex-end">
+        alignItems="center">
          <Link to="/" style={{textDecoration: 'none', color:'#3A1212', borderBottom:'3px solid #FF2625'}}>Home</Link> 
          <a href="#exercises" style={{textDecoration:'none', color :'#3A1212'}}>Exercises</a>
         </Stack>
