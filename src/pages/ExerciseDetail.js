5b38093c0ec31c3609de26517b3a3a397ec38762
Petar Zelenovic <petarzelenovic261@gmail.com>
2023-02-12 18:40:19 +0100

    dodata strana exercise detail

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material';
+import Detail from '../components/Detail';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+
+  if (!exerciseDetail) return <div>No Data</div>;
   return (
-    <div>ExerciseDetail</div>
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+    <Detail exerciseDetail={exerciseDetail} />
+    {/* <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} /> */}
+    {/* <SimilarExercises targetMuscleExercises={targetMuscleExercises} /> */}
+  </Box>
   )
 }
 
